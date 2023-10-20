cb95cf7b5c7cb67464373a34f1df63f152ab8cd6
Hashan Samarakkody <142280872+Hashan-Samarakkody@users.noreply.github.com>
2023-10-20 18:50:36 +0530

    Update

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 class Test{
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
+        
         System.out.println("Hello World");
     }
+    
+
 }
